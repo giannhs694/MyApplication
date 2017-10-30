e181e3103a4c161adee6e040dd76df3ce5c53ca4
John <giannhs694@gmail.com>
2017-10-30 14:08:09 +0200

    make http request using volley library

--- MyApplication/app/src/main/java/com/mydomaingiannis/dev/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/mydomaingiannis/dev/myapplication/MainActivity.java
@@ -2,12 +2,57 @@ package com.mydomaingiannis.dev.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSendRequest;
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "http://www.xrysessyntages.com/";
+    private static final String TAG= MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSendRequest = (Button) findViewById(R.id.btnSendRequest);
+        btnSendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //send request -> print respone (using Volley library)
+                sendRequestAndPrintResponse();
+            }
+        });
+    }
+
+    private void sendRequestAndPrintResponse() {
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i(TAG,"Response :"+ response.toString());
+                int len = response.length();
+                Log.i(TAG,  Integer.toString(len)  );
+
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG,"Error :" + error.toString());
+            }
+        });
+        mRequestQueue.add(mStringRequest);
     }
 }
