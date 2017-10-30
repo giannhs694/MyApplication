21e7d6170f15d55fdbeaa7e199c819e8a5a6a7a9
John <giannhs694@gmail.com>
2017-10-30 19:38:13 +0200

    finished html request with volley, store in a string

--- MyApplication/app/src/main/java/com/mydomaingiannis/dev/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/mydomaingiannis/dev/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -15,10 +16,13 @@ import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String REQUESTTAG = "test" ;
     private Button btnSendRequest;
     private RequestQueue mRequestQueue;
     private StringRequest mStringRequest;
-    private String url = "http://www.xrysessyntages.com/";
+    private String info="";
+    private String url="http://www.xrysessyntages.com/?"+info+"=&lang=el" ;
     private static final String TAG= MainActivity.class.getName();
 
     @Override
@@ -30,21 +34,24 @@ public class MainActivity extends AppCompatActivity {
         btnSendRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //send request -> print respone (using Volley library)
+                //send request -> print response (using Volley library)
                 sendRequestAndPrintResponse();
             }
         });
     }
 
     private void sendRequestAndPrintResponse() {
+        info = ((EditText) findViewById(R.id.searchField)).getText().toString();
         mRequestQueue = Volley.newRequestQueue(this);
 
         mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Log.i(TAG,"Response :"+ response.toString());
-                int len = response.length();
-                Log.i(TAG,  Integer.toString(len)  );
+                EditText test = ((EditText) findViewById(R.id.test));
+                test.setText(response.toString());
+                //int len = response.length();
+                Log.i(TAG,  url  );
 
             }
         }, new Response.ErrorListener(){
@@ -53,6 +60,25 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG,"Error :" + error.toString());
             }
         });
+        mStringRequest.setTag(REQUESTTAG);
         mRequestQueue.add(mStringRequest);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mRequestQueue.cancelAll(REQUESTTAG);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    mRequestQueue.cancelAll(REQUESTTAG);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mRequestQueue.cancelAll(REQUESTTAG);
     }
 }
